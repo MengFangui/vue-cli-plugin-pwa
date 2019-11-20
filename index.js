b0ffc29d9f7df239171c2c6acc7c3eb56a442d62
mengfangui <meng010387@126.com>
2019-11-20 10:05:15 +0800

    1th

---
+++ index.js
@@ -0,0 +1,32 @@
+const BasicPlugin = require('@nbjump/pwa-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+
+module.exports = (api, options) => {
+
+    if (!JSON.parse(process.env.NBJUMP_PWA || 'true')) {
+        return;
+    }
+
+    const version = options.pwa && options.pwa.version
+        ? options.pwa.version
+        : new Date().getTime().toString().substr(-6);
+
+    const cacheName = `${api.service.pkg.name}-${version}`;
+    const userOptions = Object.assign({}, options.pwa, {
+        cacheName 
+    });
+    api.chainWebpack(webpackConfig => {
+        webpackConfig
+        .plugin('basic').use(BasicPlugin, [userOptions]);
+
+        webpackConfig
+        .plugin('copy').use(CopyWebpackPlugin, [[{
+            from: `${process.cwd()}/public`,
+            to: `${process.cwd()}/dist-h5/dist`,
+            ignore: [
+                '.DS_Store'
+            ]
+        }]]);
+    });
+
+}
